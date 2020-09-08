d2cca7a73324ca2ea312ebef44ecdc16f9a7000a
Brandon Dong <gasingdong@gmail.com>
2020-09-08 16:59:53 -0700

    start up react and gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: `Rune Canvas`,
+    description: ``,
+    author: `Brandon Dong`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-sass`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
