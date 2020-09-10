109d910fee62502db0a1d71bbb036eb58be3d463
Brandon Dong <gasingdong@gmail.com>
2020-09-10 16:53:29 -0700

    render card frame with canvas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import frames from '../assets/frames.png';
 
 const Home: React.FC = () => {
-  return (
-    <div>
-    	Home Page
-    </div>
-  );
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+
+      if (ctx) {
+        const image = new Image();
+        image.onload = (): void => {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.drawImage(
+            image,
+            0,
+            0,
+            653,
+            image.height,
+            0,
+            0,
+            653 * 0.5,
+            image.height * 0.5
+          );
+        };
+        image.onerror = (e): void => console.log(e);
+        image.src = frames;
+      }
+    }
+  }, []);
+
+  return <canvas ref={canvasRef} width={700} height={1000} />;
 };
 
 export default Home;
